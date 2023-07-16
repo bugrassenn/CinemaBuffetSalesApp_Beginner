3848c69db5257b810bb32f51a123e2fd985cc0cc
Bugra Sen <bugra.sen@bil.omu.edu.tr>
2023-07-17 02:57:43 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,341 @@
+﻿namespace CinemaBuffetSalesApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.LblCorn = new System.Windows.Forms.Label();
+            this.TxtCorn = new System.Windows.Forms.TextBox();
+            this.LblWater = new System.Windows.Forms.Label();
+            this.TxtWater = new System.Windows.Forms.TextBox();
+            this.LblTea = new System.Windows.Forms.Label();
+            this.TxtTea = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.TxtTicket = new System.Windows.Forms.TextBox();
+            this.LblTicket = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.LblCaseTotal = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.LblTotal = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // LblCorn
+            // 
+            this.LblCorn.AutoSize = true;
+            this.LblCorn.Location = new System.Drawing.Point(50, 70);
+            this.LblCorn.Name = "LblCorn";
+            this.LblCorn.Size = new System.Drawing.Size(60, 23);
+            this.LblCorn.TabIndex = 0;
+            this.LblCorn.Text = "Corn : ";
+            // 
+            // TxtCorn
+            // 
+            this.TxtCorn.Location = new System.Drawing.Point(127, 67);
+            this.TxtCorn.Name = "TxtCorn";
+            this.TxtCorn.Size = new System.Drawing.Size(100, 31);
+            this.TxtCorn.TabIndex = 1;
+            // 
+            // LblWater
+            // 
+            this.LblWater.AutoSize = true;
+            this.LblWater.Location = new System.Drawing.Point(44, 107);
+            this.LblWater.Name = "LblWater";
+            this.LblWater.Size = new System.Drawing.Size(68, 23);
+            this.LblWater.TabIndex = 2;
+            this.LblWater.Text = "Water :";
+            // 
+            // TxtWater
+            // 
+            this.TxtWater.Location = new System.Drawing.Point(127, 104);
+            this.TxtWater.Name = "TxtWater";
+            this.TxtWater.Size = new System.Drawing.Size(100, 31);
+            this.TxtWater.TabIndex = 3;
+            // 
+            // LblTea
+            // 
+            this.LblTea.AutoSize = true;
+            this.LblTea.Location = new System.Drawing.Point(66, 144);
+            this.LblTea.Name = "LblTea";
+            this.LblTea.Size = new System.Drawing.Size(45, 23);
+            this.LblTea.TabIndex = 4;
+            this.LblTea.Text = "Tea :";
+            // 
+            // TxtTea
+            // 
+            this.TxtTea.Location = new System.Drawing.Point(127, 141);
+            this.TxtTea.Name = "TxtTea";
+            this.TxtTea.Size = new System.Drawing.Size(100, 31);
+            this.TxtTea.TabIndex = 5;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.groupBox1.Controls.Add(this.TxtTicket);
+            this.groupBox1.Controls.Add(this.LblTicket);
+            this.groupBox1.Controls.Add(this.TxtTea);
+            this.groupBox1.Controls.Add(this.LblTea);
+            this.groupBox1.Controls.Add(this.TxtWater);
+            this.groupBox1.Controls.Add(this.LblWater);
+            this.groupBox1.Controls.Add(this.TxtCorn);
+            this.groupBox1.Controls.Add(this.LblCorn);
+            this.groupBox1.Location = new System.Drawing.Point(106, 251);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(261, 278);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Product Amount";
+            // 
+            // TxtTicket
+            // 
+            this.TxtTicket.Location = new System.Drawing.Point(127, 178);
+            this.TxtTicket.Name = "TxtTicket";
+            this.TxtTicket.Size = new System.Drawing.Size(100, 31);
+            this.TxtTicket.TabIndex = 7;
+            // 
+            // LblTicket
+            // 
+            this.LblTicket.AutoSize = true;
+            this.LblTicket.Location = new System.Drawing.Point(39, 181);
+            this.LblTicket.Name = "LblTicket";
+            this.LblTicket.Size = new System.Drawing.Size(74, 23);
+            this.LblTicket.TabIndex = 6;
+            this.LblTicket.Text = "Ticket :  ";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.groupBox2.Controls.Add(this.label8);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Location = new System.Drawing.Point(418, 251);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(173, 278);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Price Table";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(34, 178);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(108, 23);
+            this.label8.TabIndex = 4;
+            this.label8.Text = "Ticket :  90 ₺";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(55, 141);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(87, 23);
+            this.label7.TabIndex = 3;
+            this.label7.Text = "Tea :  12 ₺";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(42, 104);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(100, 23);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "Water :  4 ₺";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(44, 67);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(98, 23);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Corn :  40 ₺";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.groupBox3.Controls.Add(this.button2);
+            this.groupBox3.Controls.Add(this.LblCaseTotal);
+            this.groupBox3.Controls.Add(this.label13);
+            this.groupBox3.Controls.Add(this.LblTotal);
+            this.groupBox3.Controls.Add(this.button1);
+            this.groupBox3.Controls.Add(this.label9);
+            this.groupBox3.Location = new System.Drawing.Point(618, 251);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(223, 278);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Case";
+            // 
+            // LblCaseTotal
+            // 
+            this.LblCaseTotal.AutoSize = true;
+            this.LblCaseTotal.Location = new System.Drawing.Point(124, 241);
+            this.LblCaseTotal.Name = "LblCaseTotal";
+            this.LblCaseTotal.Size = new System.Drawing.Size(44, 23);
+            this.LblCaseTotal.TabIndex = 6;
+            this.LblCaseTotal.Text = "00 ₺";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(16, 241);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(98, 23);
+            this.label13.TabIndex = 5;
+            this.label13.Text = "Case Total :";
+            // 
+            // LblTotal
+            // 
+            this.LblTotal.AutoSize = true;
+            this.LblTotal.Location = new System.Drawing.Point(112, 112);
+            this.LblTotal.Name = "LblTotal";
+            this.LblTotal.Size = new System.Drawing.Size(44, 23);
+            this.LblTotal.TabIndex = 4;
+            this.LblTotal.Text = "00 ₺";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(117, 181);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 34);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(45, 112);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(61, 23);
+            this.label9.TabIndex = 2;
+            this.label9.Text = "Total : ";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Pristina", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(113, 60);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(274, 39);
+            this.label10.TabIndex = 8;
+            this.label10.Text = "Cinema Buffet Sales App";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(638, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(231, 203);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(6, 181);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 34);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Clear";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.ClientSize = new System.Drawing.Size(951, 588);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.Name = "Form1";
+            this.Text = "Cinema Buffet";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label LblCorn;
+        private System.Windows.Forms.TextBox TxtCorn;
+        private System.Windows.Forms.Label LblWater;
+        private System.Windows.Forms.TextBox TxtWater;
+        private System.Windows.Forms.Label LblTea;
+        private System.Windows.Forms.TextBox TxtTea;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox TxtTicket;
+        private System.Windows.Forms.Label LblTicket;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label LblCaseTotal;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label LblTotal;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CinemaBuffetSalesApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        int casetotal = 0;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int corn, ticket, water, tea, sum;
+            corn = Convert.ToInt16(TxtCorn.Text);
+            ticket = Convert.ToInt16(TxtTicket.Text);
+            water = Convert.ToInt16(TxtWater.Text);
+            tea = Convert.ToInt16(TxtTea.Text);
+
+            sum = corn * 40 + water * 4 + tea * 12 + ticket * 90;
+            LblTotal.Text = sum.ToString() +  " ₺ ";
+
+
+            casetotal = casetotal + sum;
+            LblCaseTotal.Text = casetotal.ToString() + " ₺ ";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            TxtCorn.Text = "";
+            TxtTea.Text = "";
+            TxtTicket.Text = "";
+            TxtWater.Text = "";
+            LblTotal.Text = "0 ₺";
+            TxtCorn.Focus();
+        }
+    }
+}
